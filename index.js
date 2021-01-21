412eacf8ac04d7a583e4e2e88104c631322d4db1
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-01-21 16:42:20 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /*MAKE SURE TO RETURN ALL OF THE ANSWERS ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
 
+import { resolvePreset } from "@babel/core";
+
 /*When doing these tasks, we recommend using console.log to test the output of your code to make sure it works correctly.*/
 
 ///////////////Menu Items (MVP)///////////////////
@@ -15,8 +17,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(nameIn, priceIn, categoryIn){
+  let obj ={name:nameIn, price:priceIn, category:categoryIn,};
+  return obj;
+
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -28,7 +32,10 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
+let burgers = createMenuItem("Burgers", 5.50, "Lunch");
+console.log(burgers);
+console.log(createMenuItem("Grilled Cheese", 4.00, "Lunch"));
+console.log(createMenuItem("Gyro Platter", 5.00, "Dinner"));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -48,7 +55,11 @@ export const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  /*Your code here*/
+  discount: function(type){
+    let pmulti = 0;
+    type.toUpperCase() === "teacher".toUpperCase() || type.toUpperCase() === "student".toLocaleUpperCase() ? pmulti = 0.75 : type.toUpperCase() === "public".toLowerCase() ? pmulti = 0.90 : null;
+    return this.price * pmulti;
+  }
 }
 
 
@@ -72,12 +83,22 @@ Using the reviews array above:
 
 
 
+for (let i = 0; i < reviews.length; i++){
+  reviews[i].name === "Julius" ? console.log(reviews[i].feedback) : null;
+}
+
+
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Using the reviews array above do the following: (no function needed) 
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+
+  reviews.push({name:"Mind you damn business",rating:0,feedback:"Nobody gets a perfect rating on my watch"});
+
+  console.log(reviews);
 
 
 
@@ -87,8 +108,11 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
-
+for (let i = 0; i < reviews.length; i++){
+  reviews[i].name === "Reyna" ? reviews[i].feedback = "this place is chill with really cool people, great for getting work done on weekdays" : null;
+}
+console.log(reviews[7]);
+ 
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -102,8 +126,10 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, index) {
+    let theReturn = "";
+    index <= array.length ? theReturn = `${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was: ${array[index].feedback}`  : console.log(`${index} is outside of the array index range`);
+    return theReturn;
 }
 
 
@@ -121,8 +147,9 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  let last = array[array.length-1];
+  return `${last.name} gave the restaurant a ${last.rating} star review, and their feedback was: ${last.feedback}`
 } 
 
 
@@ -143,9 +170,16 @@ Use the getReviewsByRating function below to do the following:
   ]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
+ function getReviewByRating(array, rNum) {
+    let theReturnArray = [];
+    for(let i = 0; i < array.length; i++){
+      array[i].reviews.includes(rNum) ? theReturnArray.push(array[i]) : null; 
+    }
+    console.log(theReturnArray);
+    return theReturnArray;
   }
+
+  getReviewByRating(reviews, 4);
 
   
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪💪💪💪💪   
